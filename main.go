7bdbb527452829068b918fdc6b1deb71b0f83fb6
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-03-23 19:36:05 +0300

    - Implemented API JWT Authorization functionality

--- main.go
+++ main.go
@@ -1,184 +1,105 @@
 package main
 
 import (
-    "database/sql"
-    "github.com/foolin/gin-template"
-    "github.com/gin-gonic/gin"
-    "log"
-    "net/http"
-
-    _ "github.com/go-sql-driver/mysql"
+    "encoding/json"
+    "fmt"
+    "github.com/dgrijalva/jwt-go"
     _ "github.com/gin-gonic/gin"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/gorilla/context"
+    "github.com/gorilla/mux"
+    "github.com/mitchellh/mapstructure"
+    "net/http"
+    "strings"
 )
 
-type Employee struct {
-    Id    int
-    Name  string
-    City string
+
+type User struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
 }
 
-func dbConn() (db *sql.DB) {
-    dbDriver := "mysql"
-    dbUser := "root"
-    dbPass := "root"
-    dbName := "goblog"
-    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-    if err != nil {
-        panic(err.Error())
-    }
-    return db
+type JwtToken struct {
+    Token string `json:"token"`
 }
 
-func New(ctx *gin.Context) {
-    ctx.HTML(http.StatusOK, "new", nil)
-}
-func Delete(ctx *gin.Context) {
-    db := dbConn()
-    emp := ctx.Request.URL.Query().Get("id")
-    delForm, err := db.Prepare("DELETE FROM employee WHERE id=?")
-    if err != nil {
-        panic(err.Error())
-    }
-    delForm.Exec(emp)
-    log.Println("DELETE")
-    defer db.Close()
-    ctx.Redirect(http.StatusMovedPermanently, "/")
+type Exception struct {
+    Message string `json:"message"`
 }
 
-func Show(ctx *gin.Context) {
-    db := dbConn()
-    nId := ctx.Request.URL.Query().Get("id")
-    selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
-    if err != nil {
-        panic(err.Error())
-    }
-    emp := Employee{}
-    for selDB.Next() {
-        var id int
-        var name, city string
-        err = selDB.Scan(&id, &name, &city)
-        if err != nil {
-            panic(err.Error())
-        }
-        emp.Id = id
-        emp.Name = name
-        emp.City = city
-    }
-    ctx.HTML(http.StatusOK, "show", gin.H{
-        "user": emp,
+func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
+    var user User
+    user.Username="maksim"
+    user.Password="maksimqwerty"
+    _ = json.NewDecoder(req.Body).Decode(&user)
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "username": user.Username,
+        "password": user.Password,
     })
-    defer db.Close()
+    tokenString, error := token.SignedString([]byte("secret"))
+    if error != nil {
+        fmt.Println(error)
+    }
+    json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
 }
 
-func Edit(ctx *gin.Context) {
-    db := dbConn()
-    nId := ctx.Request.URL.Query().Get("id")
-    selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
-    if err != nil {
-        panic(err.Error())
-    }
-    emp := Employee{}
-    for selDB.Next() {
-        var id int
-        var name, city string
-        err = selDB.Scan(&id, &name, &city)
-        if err != nil {
-            panic(err.Error())
+func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
+    return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+        authorizationHeader := req.Header.Get("authorization")
+        if authorizationHeader != "" {
+            bearerToken := strings.Split(authorizationHeader, " ")
+            if len(bearerToken) == 2 {
+                token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+                    if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+                        return nil, fmt.Errorf("There was an error")
+                    }
+                    return []byte("secret"), nil
+                })
+                if error != nil {
+                    json.NewEncoder(w).Encode(Exception{Message: error.Error()})
+                    return
+                }
+                if token.Valid {
+                    context.Set(req, "decoded", token.Claims)
+                    next(w, req)
+                } else {
+                    json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+                }
+            }
+        } else {
+            json.NewEncoder(w).Encode(Exception{Message: "An authorization header is required"})
         }
-        emp.Id = id
-        emp.Name = name
-        emp.City = city
-    }
-    ctx.HTML(http.StatusOK, "edit", gin.H{
-        "user": emp,
     })
-    defer db.Close()
+}
+func TestEndpoint(w http.ResponseWriter, req *http.Request) {
+    decoded := context.Get(req, "decoded")
+    var user User
+    mapstructure.Decode(decoded.(jwt.MapClaims), &user)
+    json.NewEncoder(w).Encode(user)
 }
 
-func Insert(ctx *gin.Context) {
-    db := dbConn()
-    name := ctx.PostForm("name")
-    city := ctx.PostForm("city")
-    insForm, err := db.Prepare("INSERT INTO employee(name, city) VALUES(?,?)")
-    if err != nil {
-        panic(err.Error())
-    }
-    insForm.Exec(name, city)
-    log.Println("INSERT: Name: " + name + " | City: " + city)
-
-    defer db.Close()
-    ctx.Redirect(http.StatusMovedPermanently, "/")
-}
-
-func Update(ctx *gin.Context) {
-    db := dbConn()
-
-    name := ctx.PostForm("name")
-    city := ctx.PostForm("city")
-    id := ctx.PostForm("uid")
-    insForm, err := db.Prepare("UPDATE employee SET name=?, city=? WHERE id=?")
-    if err != nil {
-        panic(err.Error())
-    }
-    insForm.Exec(name, city, id)
-    log.Println("UPDATE: Name: " + name + " | City: " + city)
-
-    defer db.Close()
-    ctx.Redirect(http.StatusMovedPermanently, "/")
-}
-
-func Index(ctx *gin.Context) {
-    db := dbConn()
-    selDB, err := db.Query("SELECT * FROM employee ORDER BY id DESC")
-    if err != nil {
-        panic(err.Error())
-    }
-    emp := Employee{}
-    res := []Employee{}
-    for selDB.Next() {
-        var id int
-        var name, city string
-        err = selDB.Scan(&id, &name, &city)
-        if err != nil {
-            panic(err.Error())
+func ProtectedEndpoint(w http.ResponseWriter, req *http.Request) {
+    params := req.URL.Query()
+    token, _ := jwt.Parse(params["token"][0], func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, fmt.Errorf("There was an error")
         }
-        emp.Id = id
-        emp.Name = name
-        emp.City = city
-        res = append(res, emp)
-    }
-
-    ctx.HTML(http.StatusOK, "index", gin.H{
-        "res": res,
+        return []byte("secret"), nil
     })
-    defer db.Close()
+    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+        var user User
+        mapstructure.Decode(claims, &user)
+        json.NewEncoder(w).Encode(user)
+    } else {
+        json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+    }
 }
 
 func main() {
-    //-- Enable debug color in console
-    gin.ForceConsoleColor()
-    router := gin.Default()
-
-    //new template engine
-    router.HTMLRender = gintemplate.Default()
-
-    router.GET("/test", func(ctx *gin.Context) {
-        //render with master
-        ctx.HTML(http.StatusOK, "test", gin.H{
-            "title": "Index title!",
-            "add": func(a int, b int) int {
-                return a + b
-            },
-        })
-    })
-
-    router.GET("/", Index)
-    router.GET("/edit", Edit)
-    router.POST("/update", Update)
-    router.GET("/show", Show)
-    router.GET("/delete", Delete)
-    router.GET("/new", New)
-    router.POST("/insert", Insert)
-
-    router.Run(":9090")
+    router := mux.NewRouter()
+    fmt.Println("Starting the application...")
+    router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
+    router.HandleFunc("/protected", ProtectedEndpoint).Methods("GET")
+    router.HandleFunc("/test", ValidateMiddleware(TestEndpoint)).Methods("GET")
+    http.ListenAndServe(":9090", router)
 }
